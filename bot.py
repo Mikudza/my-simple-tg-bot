dc4e9387374093e5e411bc236c1fc23efe4e515c
Gujicov Artiom <a.gujicov@gmail.com>
2022-02-11 15:02:43 +0200

    Edit telegram-bot service and check script

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
-import config   # use config.py to store your bot token in TOKEN var
+import config  # use config.py to store your bot token in TOKEN var
 import telebot  # Telegram Bot API
 
-
 bot = telebot.TeleBot(config.TOKEN)
+
+
+def send_message(message_text):
+    bot.send_message(config.MY_ID, message_text)
+
 
 # Run
 if __name__ == "__main__":
     @bot.message_handler(commands=['start', 'id'])
     def send_id(message):
         bot.send_message(message.chat.id, message.from_user.id)
+
+
     bot.polling(non_stop=True)
---
+++ log-checker.py
@@ -0,0 +1,9 @@
+from bot import send_message
+
+
+
+"""
+There wil be a block that compile message from log of unsuccessful attempts
+"""
+message = "тест"
+send_message(message)
